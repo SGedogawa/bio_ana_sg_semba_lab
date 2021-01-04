52b85f3501745277b57d38f5b3ffabb5a6475ea5
sgedogawa <rs0723new@gmail.com>
2021-01-05 03:45:35 +0900

    from note sg

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from mylib import calc
+
+import pandas as pd
+from gtfparse import read_gtf
 
 if __name__ == '__main__':
-    x = 2
-    print(x)
+    ima = calc.Calc().find_r_gene(3.0)
+    print(len(ima))
